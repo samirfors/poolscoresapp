5dfef03c99003712ba915baba1b719e6e0a14f01
Chiristian Östman <christian.ostman@gmail.com>
2012-10-20 19:44:12 +0200

    Database connect

--- js/collections/players_collection.js
+++ js/collections/players_collection.js
@@ -12,12 +12,36 @@ function(player_model) {
 
     getAllPlayers: function() {
       var self = this;
-      $.getJSON('json/players.json', function(data) {
+     /* $.getJSON('json/players.json', function(data) {
         _.each(data.players, function(player) {
           self.push(new player_model({id:player.id, name:player.name}));
         });
+      });*/
+      var Player = Parse.Object.extend("Player");
+      var query = new Parse.Query(Player);
+    //  query.equalTo("playerName", "Dan Stemkoski");
+      query.find({
+        success: function(results) {
+          //alert("Successfully retrieved " + results.length + " players.");
+
+          for(var i=0; i < results.length; i++)
+          {
+            //console.log(results[i].get("name"));
+            console.log(results[i].id);
+            self.push(new player_model({id:results[i].id, name:results[i].get("name")}));
+
+          }
+
+
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
       });
+
+
     }
+
   });
 
   return Players;
