a06332e6c2bc8b54e1c7a8ed4f649c68de29023c
Chiristian Östman <christian.ostman@gmail.com>
2012-10-23 17:31:50 +0200

    Första version av scheduleprocessor för att generera matcher

--- js/models/fixture_model.js
+++ js/models/fixture_model.js
@@ -1,4 +1,5 @@
-define([],
+define([ 'models/fixture_model'
+],
 function() {
 
   var Fixture = Backbone.Model.extend({
@@ -9,8 +10,21 @@ function() {
       homePoints  : 0,
       awayPoints  : 0,
       homeCunts   : 0,
-      awayCunts   : 0
-    }
+      awayCunts   : 0,
+    }, 
+
+    rotate:function(){
+        console.log("Rot")
+        var h1 = this.get("home");
+        var a1 = this.get("away");
+
+        this.set("home",a1);
+        this.set("away",h1);
+      }
+
+
+
+
   });
 
   return Fixture;
--- js/models/tournament_model.js
+++ js/models/tournament_model.js
@@ -12,8 +12,95 @@ function(fixtures_collection,fixture_model) {
       players: []
     },
 
+
     initialize: function() {
+
+      // TODO: break out in separate module/helper to easily switch
+
+      this.scheduleProcessor = {
+        process:function(players,rounds){
+            console.log("process")
+            var prio = {};
+            var fixtures = new fixtures_collection();
+
+            for (var i in players)
+            {
+              players[i].prio = 0;
+            }
+
+            // Create first round that then gets rotated for the remaining rounds
+            var baseRound = [];
+
+            var gamesPerRound = (players.length-1) * ((players.length) / 2)
+            //alert(gamesPerRound);
+            
+
+            for(var i = 0; i < gamesPerRound; i++)
+            {
+              var p1 = players[0];
+              var p2;
+              // FIND A PLAYER p1 hasn't played
+              for(var j=1; j<players.length; j++)
+              {
+                  var otherPlayer = players[j]
+                  var hasPlayed = false;
+                  for(var f in baseRound)
+                  {
+
+                    if(baseRound[f].get("home") == p1 && baseRound[f].get("away") == otherPlayer) hasPlayed = true;
+                    if(baseRound[f].get("away") == p1 && baseRound[f].get("home")  == otherPlayer) hasPlayed = true;
+                  }
+
+                  if(hasPlayed == false)
+                  {
+                    if(!p2)
+                    p2 = otherPlayer;
+                    else if(otherPlayer.prio > p2.prio) p2 = otherPlayer
+                  }
+              }
+
+              var newFixture = new fixture_model({home:p1,away:p2})
+              baseRound.push(newFixture);
+             
+              
+
+              p1.prio-=1.5;
+              p2.prio-=1;
+              players.sort(sortOnPrio)
+            }
+
+            // Reverse baserRund to make sure player[0] plays last game and add it to the fixtures
+            baseRound = baseRound.reverse();
+           for(var i in baseRound)
+            {
+               fixtures.push(baseRound[i])
+            }
+
+            // REPEAT FOR REMAINING ROUNDS
+            for(var i = 1; i < rounds; i++)
+            {
+              for(var fixt in baseRound)
+              {
+                var newFixt = baseRound[fixt].clone()
+                if(i%2 != 0) newFixt.rotate();
+
+                fixtures.push(newFixt)
+              }
+            }
+
+          function sortOnPrio(a, b){
+            if(a.prio > b.prio) return -1;
+
+            return 1;
+          }
+
+          return fixtures;
+
+        }
+      }
     },
+
+
 
     addPlayer: function(player) {
       var players = this.get('players');
@@ -24,23 +111,23 @@ function(fixtures_collection,fixture_model) {
     },
 
     generateMatchSchedule: function() {
-      var players = this.get('players'),
-          fixtures = this.get('fixtures'),
-          i,
-          rounds = 2,
-          p1,
-          p2;
+      var rounds = 2;
+      var players = this.get('players');
+      
+      if(players.length < 2) return false;
+      else if(players.length == 2) rounds = 6;
+      else if(players.length == 3) rounds = 3;
+      else rounds = 2;
 
-      for (i=0; i < players.length * rounds;i++) {
-        p1 = players[Math.floor(Math.random()*players.length)];
-        p2 = p1;
-        while(p1 === p2) {
-          p2 = players[Math.floor(Math.random()*players.length)];
-        }
-        fixtures.push(new fixture_model({home:p1, away:p2}));
-      }
-
+      this.set("fixtures", this.scheduleProcessor.process(this.get('players'),rounds));
+      // return false if it failed
       return true;
+
+    },
+
+    saveTournament:function(){
+      //Using parse 
+
 
     }
 
